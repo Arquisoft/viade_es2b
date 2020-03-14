a7621844a286e7c9577ecc38e2deb60b84c17b90
Aini <angela_l.lopez@hotmail.com>
2020-03-14 16:07:56 +0100

    fix

--- src/components/NavBar/children/Navigation/navigation.test.js
+++ src/components/NavBar/children/Navigation/navigation.test.js
@@ -31,6 +31,6 @@ describe.only('Navigation', () => {
     const item = getByTestId('item');
 
     expect(item.children.length).toBe(1);
-    expect(item).toHaveTextContent('Home');
+    expect(item).toHaveTextContent('home');
   });
 });
