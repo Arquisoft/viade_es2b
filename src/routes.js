2b4a92c5d86f4f77955bd161498281f0dacca1ca
Aini <angela_l.lopez@hotmail.com>
2020-03-16 12:45:12 +0100

    > deleted

--- src/routes.js
+++ src/routes.js
@@ -25,7 +25,7 @@ const privateRoutes = [
 ];
 
 const Routes = () => {
-  return (<Router basename={'/viade_es2b'}>>
+  return (<Router basename={'/viade_es2b'}>
     <Fragment>
       <Switch>
         <NotLoggedInLayout component={Login} path="/login" exact />
