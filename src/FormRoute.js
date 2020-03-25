5e62ddb389ce737edacef3ed29a6524bd7287726
Daniel Fernández Aller <daniferna@outlook.com>
2020-03-25 15:00:12 +0100

    Working form for name and description

--- src/FormRoute.js
+++ src/FormRoute.js
@@ -5,42 +5,64 @@ import Button from 'react-bootstrap/Button'
 export default class RouteForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { form: {name: "", description: "", gpx: null, images: []} };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    let fieldName = event.target.name;
+    let fieldVal = event.target.value;
+    this.setState({form: {...this.state.form, [fieldName]: fieldVal}});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('A name was submitted: ' + this.state.form.name +
+     '\nA description was submitted: ' + this.state.form.description);
     event.preventDefault();
   }
 
   render() {
     return (
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-    </Form.Text>
+      <Form onSubmit={this.handleSubmit}>
+
+        <Form.Group controlId="formNameRoute">
+          <Form.Label>Route name</Form.Label>
+          <Form.Control type="text" name="name" placeholder="Enter name"
+           defaultValue={this.state.form.name} onChange={this.handleChange} />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+        <Form.Group controlId="formDescriptionRoute">
+          <Form.Label>Route description</Form.Label>
+          <Form.Control type="text" name="description" placeholder="Enter route description"
+            defaultValue={this.state.form.description} onChange={this.handleChange} />
         </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+
+        <Form.Group controlId="formGpxFile">
+          <Form.Label>GPX file</Form.Label>
+          <Form.File
+            id="gpx-file"
+            label="Load gpx file"
+            custom
+          >
+          </Form.File>
         </Form.Group>
+
+        <Form.Group controlId="formImages">
+          <Form.Label>Route images</Form.Label>
+          <Form.File
+            id="gpx-file"
+            label="Load gpx file"
+            custom
+            multiple
+          >
+          </Form.File>
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           Submit
-  </Button>
+        </Button>
       </Form>
     );
   }
