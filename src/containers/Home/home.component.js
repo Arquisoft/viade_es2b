3da470273af6d4dc0876baed9a42737ddb522515
Iván Fernández López <56384539+uo265349@users.noreply.github.com>
2020-04-10 21:52:43 +0200

    Hotfix: added import for CSS file for leaflet map

--- src/containers/Home/home.component.js
+++ src/containers/Home/home.component.js
@@ -11,6 +11,7 @@ import {
   RouteInfo
 } from './home.style';
 import RouteList from './components/RouteList';
+import 'leaflet/dist/leaflet.css';
 const Map = React.lazy(() => import('../../Map'));
 
 /**
