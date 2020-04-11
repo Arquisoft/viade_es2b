dd1877cd5557f2301a2189815c6034b2e4ec4f53
Daniel Fernández Aller <daniferna@outlook.com>
2020-04-11 17:27:54 +0200

    Now while loading a progress indicator appears

--- src/containers/Home/home.component.js
+++ src/containers/Home/home.component.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 //import { Uploader } from '@inrupt/solid-react-components';
 import { useTranslation } from 'react-i18next';
+import { Button } from '@material-ui/core';
 import manejadorPODs from '../../persistanceManagement'
 import {
   HomeWrapper,
@@ -10,9 +11,10 @@ import {
   RouteMap,
   RouteInfo
 } from './home.style';
-import RouteList from './components/RouteList';
 import 'leaflet/dist/leaflet.css';
+
 const Map = React.lazy(() => import('../../Map'));
+const RouteList = React.lazy(() => import('./components/RouteList'))
 
 /**
  * Hom Page UI component, containing the styled components for the Hom Page
@@ -26,18 +28,22 @@ export const HomePageContent = props => {
     <HomeWrapper data-testid="home-wrapper">
       <HomeSidenav className="home-sidebar">
         <h2>Rutas</h2>
-        <Suspense fallback={<div>{t('home.loading_routes')}</div>}>
-          <RouteList setRoute = {props.setRoute} ></RouteList>
-        </Suspense>
-        <button onClick={() => manejadorPODs.deleteRoutes()}>
-          Eliminar rutas
-        </button>
+        <RouteList loadingText={t("home.loading_routes")} setRoute={props.setRoute} ></RouteList>
+        <div id="Manage buttons">
+          <Button variant="contained" color="primary" onClick={() => ""}>
+            Añadir ruta
+          </Button>
+          <span>   </span>
+          <Button variant="contained" color="secondary" onClick={() => manejadorPODs.deleteRoutes()}>
+            Eliminar rutas
+        </Button>
+        </div>
       </HomeSidenav>
       <HomeBody className="home-body">
         <HomeCard className="card">
           <RouteMap id="map">
             <Suspense fallback={<div>{t('home.loading_routes')}</div>}>
-              <Map gpx = { props.routeGPX }></Map>
+              <Map gpx={props.routeGPX}></Map>
             </Suspense>
           </RouteMap>
           <RouteInfo>
