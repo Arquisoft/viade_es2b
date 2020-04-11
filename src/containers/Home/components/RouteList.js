d820fec1a4463e5180f7cc3a522ec2d77bb7d1f7
Daniel Fernández Aller <daniferna@outlook.com>
2020-04-11 17:36:54 +0200

    Fixed error in merging

--- src/containers/Home/components/RouteList.js
+++ src/containers/Home/components/RouteList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IconButton, CircularProgress } from '@material-ui/core';
+import { Button, IconButton, CircularProgress } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import gestorPOD from '../../../persistanceManagement';
@@ -31,7 +31,7 @@ export default class RouteList extends React.Component {
                 <ul>
                     {this.state.routes.map(route => (
                         <li id="container_route" key={route.id}>
-                            <a href="#routes"> {route.name} </a>
+                            <Button color="primary" onClick={() => this.props.setRoute(route)}> {route.name} </Button>
                             <IconButton onClick={() => { Promise.resolve(gestorPOD.deleteRoute(route.id)).then(() => window.location.reload) }} aria-label="delete">
                                 <DeleteIcon fontSize="small" />
                             </IconButton>
