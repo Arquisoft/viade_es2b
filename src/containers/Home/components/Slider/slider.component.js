bf3c7f95fa5473acaa8c3f49ca8d1464b6e932f0
UO263662 <UO263662@uniovi.es>
2020-04-11 21:36:22 +0200

    Only 6+ images will show the left or right buttons of the slider

--- src/containers/Home/components/Slider/slider.component.js
+++ src/containers/Home/components/Slider/slider.component.js
@@ -4,7 +4,6 @@ import { SliderWrapper, ImageWrapper, ImageContainer } from './Slider.style';
 const Slider = props => {
     const [count, setCount] = useState(0);
     const images = props.imgs;
-    const [nElements] = useState(images === undefined ? 0 : images.length);
 
     const clickLeft = () => {
         if(count > 0)
@@ -12,20 +11,24 @@ const Slider = props => {
     }
 
     const clickRight = () => {
-        if(count < nElements-5)
+        if(count < images.length-5)
             setCount(count + 1);
     }
 
     return (images === undefined ? <SliderWrapper></SliderWrapper> :
         <SliderWrapper>
-            <button id="left" onClick={clickLeft}>&lt;</button>
+            {images.length > 5 &&
+                <button id="left" onClick={clickLeft}>&lt;</button>
+            }
             {images.slice(count, count+5).map(image => 
                     <ImageContainer>
                         <ImageWrapper src={URL.createObjectURL(image)} alt="imagen"/>
                     </ImageContainer>
                 )
             }
-            <button id="right" onClick={clickRight}>&gt;</button>
+            {images.length > 5 &&
+                <button id="right" onClick={clickRight}>&gt;</button>
+            }
         </SliderWrapper>
     );
 };
