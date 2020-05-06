3ed87e266294052edd6f79b8e4a2c6d2b68a645b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-06 20:57:37 +0000

    Deploy Arquisoft/viade_es2b to github.com/Arquisoft/viade_es2b.git:gh-pages

---
+++ precache-manifest.aa1dae068de600777d4aab68a63dfcac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c8462beded347a06c0",
+    "url": "https://arquisoft.github.io/viade_es2b/static/css/main.1098520e.chunk.css"
+  },
+  {
+    "revision": "65c8462beded347a06c0",
+    "url": "https://arquisoft.github.io/viade_es2b/static/js/main.65c8462b.chunk.js"
+  },
+  {
+    "revision": "9b0c13d20c08e515c52f",
+    "url": "https://arquisoft.github.io/viade_es2b/static/js/runtime~main.9b0c13d2.js"
+  },
+  {
+    "revision": "2804220c1f7e751e9a06",
+    "url": "https://arquisoft.github.io/viade_es2b/static/css/2.de288008.chunk.css"
+  },
+  {
+    "revision": "2804220c1f7e751e9a06",
+    "url": "https://arquisoft.github.io/viade_es2b/static/js/2.2804220c.chunk.js"
+  },
+  {
+    "revision": "24aaf93b3a13b70092e6",
+    "url": "https://arquisoft.github.io/viade_es2b/static/js/3.24aaf93b.chunk.js"
+  },
+  {
+    "revision": "2bcbf0bd7f169d40273d",
+    "url": "https://arquisoft.github.io/viade_es2b/static/js/4.2bcbf0bd.chunk.js"
+  },
+  {
+    "revision": "75128cb0d6d08c9f66c1cf7f912648ed",
+    "url": "https://arquisoft.github.io/viade_es2b/popup.html"
+  },
+  {
+    "revision": "35a9c0cff2bb82890b850f239c1f21b2",
+    "url": "https://arquisoft.github.io/viade_es2b/solid-auth-client.bundle.js"
+  },
+  {
+    "revision": "efe74771603c09eb26fb6333bdc55a38",
+    "url": "https://arquisoft.github.io/viade_es2b/solid-query-ldflex.bundle.js"
+  },
+  {
+    "revision": "0f4d2d16c0c6904d1d0d1b950a24fef4",
+    "url": "https://arquisoft.github.io/viade_es2b/index.html"
+  }
+];
